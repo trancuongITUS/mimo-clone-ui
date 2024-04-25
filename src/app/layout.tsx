75a3fb52c2daf990427291535e3ad038d01e9e46
Tran Quang Phat <phattran.2002.work@gmail.com>
2024-04-25 08:03:27 +0700

    Handle Update user info to Firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Toaster />
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
