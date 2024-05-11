6dcc1d1dc4a420ebbe44e8599ed44dcbd6874483
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2024-05-12 00:50:02 +0700

    Admin panel search + specific user edit data page
    Using search input with add params to the page for query data from database. Make a specific user page using id. The admin can edit data, lock or delete the data's account with this page.

---
+++ src/components/admin/static-data.tsx
@@ -0,0 +1,28 @@
+import { User } from "@/components/admin/user-table";
+
+export const usersData: User[] = [
+    {
+        id: '1',
+        name: "John Doe",
+        email: "john@example.com",
+        username: "johndoe123"
+    },
+    {
+        id: '2',
+        name: "Jane Smith",
+        email: "jane@example.com",
+        username: "janesmith456"
+    },
+    {
+        id: '3',
+        name: "Alice Johnson",
+        email: "alice@example.com",
+        username: "alicej"
+    },
+    {
+        id: '4',
+        name: "Bob Brown",
+        email: "bob@example.com",
+        username: "bobbrown789"
+    }
+];
